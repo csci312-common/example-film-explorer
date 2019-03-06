aaccb0bc15b1c2daa608f8da29ac99e30dda6341
Christopher Andrews <candrews@middlebury.edu>
2019-03-05 22:45:45 -0500

    Adds input validation and periodic refreshing.

--- server-memory/index.js
+++ server-memory/index.js
@@ -31,19 +31,29 @@ const corsOptions = {
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 
-const movies = {};
+let movies;
+const cleanMovies = new Map();
 
 app.get('/api/movies', (request, response) => {
-  response.send(Object.values(movies));
+  response.send(Array.from(movies.values()));
 });
 
 app.get('/api/movies/:id', (request, response) => {
-  response.send(movies[request.params.id]);
+  response.send(movies.get(request.params.id));
 });
 
 app.put('/api/movies/:id', (request, response) => {
-  movies[request.params.id] = request.body;
-  response.send(request.body);
+  const movieId = parseInt(request.params.id, 10);
+  const newMovie = request.body;
+  if (newMovie.id && newMovie.id === movieId && movies.get(movieId)) {
+    const mergedMovie = Object.assign({}, movies.get(movieId), newMovie);
+    movies.set(mergedMovie.id, mergedMovie);
+    response.send(mergedMovie);
+  } else {
+    console.error(`Received bad movie item [${movieId}]`);
+    console.error(request.body);
+    response.status(500).send({ error: 'Invalid movie object received' });
+  }
 });
 
 // Load movie data before starting the server
@@ -51,12 +61,18 @@ app.put('/api/movies/:id', (request, response) => {
 fs.readFile(path.join(__dirname, 'movies.json'), (err, contents) => {
   const data = JSON.parse(contents);
   data.forEach(movie => {
-    movies[movie.id] = movie;
+    cleanMovies.set(movie.id, movie);
   });
+  movies = new Map(cleanMovies);
+  // reset the movie collection periodically
+  setInterval(() => {
+    movies = new Map(cleanMovies);
+  }, 300000); // reset the movies every five minutes
 
   // Don't start server until data loaded
   // We create the server explicitly (instead of using app.listen()) to
   // provide an example of how we would create a https server
-  const server = http.createServer(app).listen(process.env.PORT || 3001);
-  console.log('Listening on port %d', server.address().port);
+  const server = http.createServer(app).listen(process.env.PORT || 3001, () => {
+    console.log('Listening on port %d', server.address().port);
+  });
 });
